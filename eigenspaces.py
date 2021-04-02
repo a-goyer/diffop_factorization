416e2200d0ce480c768ed9d86939ee717274f046
a-goyer <alexandre.goyer@inria.fr>
2021-04-02 16:32:38 +0200

    function gen_eigenspace (bug with projections)

--- eigenspaces.py
+++ eigenspaces.py
@@ -1,7 +1,9 @@
 from .precision_error import PrecisionError
 from .polynomials import radical, XGCD
-
-
+from .reduction import ker
+from sage.matrix.special import identity_matrix
+from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+from sage.misc.misc_c import prod
 
 def eigenvalues(mat, multiplicities=False):
 
@@ -40,25 +42,25 @@ def eigenvalues(mat, multiplicities=False):
 
     n = mat.nrows()
     derivatives = [p]
-    for i in range(n-1):
+    for i in range(n):
         p = p.derivative()
         derivatives.append(p)
 
     for j, ev in enumerate(s):
         m = 1
         evaluations = [pol(ev) for pol in derivatives]
-        while evaluations[m-1] == 0:
+        while evaluations[m].contains_zero():
             m = m + 1
         s[j] = (ev, m)
 
-    if n > sum(m for _, m in s):
+    if sum(m for _, m in s)<n:
         raise PrecisionError("Cannot compute multiplicities.")
 
     return s
 
 
 
-def gen_eigspaces(mat, projections=False):
+def gen_eigenspaces(mat, projections=False):
 
     r"""
     Return the generalized eigenspaces of "mat".
@@ -66,8 +68,8 @@ def gen_eigspaces(mat, projections=False):
     Note: this function is designed for ComplexBallField as base ring.
 
     Some words about the correction of this algorithm:
-    Let GenEigSpaces be the output for gen_eigspaces(mat, projections=True). For
-    any mat· in mat, there is a selection [space1·, ..., spacek·] in
+    Let GenEigSpaces be the output for gen_eigenspaces(mat, projections=True).
+    For any mat· in mat, there is a selection [space1·, ..., spacek·] in
     GenEigSpaces=[space1, ..., spacek] where each spacei· is a selection
     {'eigenvalue' : li·, 'multiplicity' : mi, 'eigenvectors' : bi·,
     'projection' : pi·} in spacei, such that:
@@ -93,7 +95,7 @@ def gen_eigspaces(mat, projections=False):
     "mat", whose keys are the following strings:
      - 'eigenvalue'   : complex number
      - 'multiplicity' : integer
-     - 'eigenvectors' : list of vectors
+     - 'basis' : list of vectors
      - 'projection'   : polynomial (if 'projections=True' is specified).
 
 
@@ -101,43 +103,26 @@ def gen_eigspaces(mat, projections=False):
     """
 
     n, C = mat.nrows(), mat.base_ring()
+    I = identity_matrix(C, n)
+    Pol, x = PolynomialRing(C, 'x').objgen()
 
     s = eigenvalues(mat, multiplicities=True)
 
+    if projections:
+        k, l = len(s), len(s)//2
+        P = [(x-ev)*m for ev, m in s]
+        Q = [Pol(prod(P[j] for j in range(k) if j != i)) for i in range(k)]
+        d, u1, u2 = XGCD(sum(Q[:l]), sum(Q[l:]))
+        proj = [u*q for u, q in zip([u1]*l + [u2]*(k-l), Q)]
+
     GenEigSpaces = []
-
-
-
-
-
-    """
-    Pol, x = PolynomialRing(C, 'x').objgen()
-
-    # call 'gen_eigspaces' function
-    GenEigSpaces = gen_eigspaces(mat)
-    s = len(GenEigSpaces)
-    if s == 1: return [tuple(list(GenEigSpaces[0])+[Pol(1)])]
-
-    # compute cofactors
-    P = [(x-eig)**mult for eig, mult, _ in GenEigSpaces]
-    Q = [prod(P[j] for j in range(s) if j != i) for i in range(s)]
-    r = s//2
-    D, U1, U2 = xgcd(sum(Q[:r]), sum(Q[r:]))
-    U = [U1]*r + [U2]*(s-r)
-
-    dec = [space + (u*q,) for space, u, q in zip(GenEigSpaces, U, Q)]
-
-
-
-
-
-
-    # computation of a basis of the corresponding generalized eigenspace
-    b = ((mat - ev*identity_matrix(C, nrows))**m).right_kernel().basis()
-    if len(b) != m:
-        raise PrecisionError("Cannot compute a basis of this generalized eigenspace.")
-    b = [V(u) for u in b]
-
-    GenEigSpaces.append((ev, m, b))"""
+    for i, (ev, m) in enumerate(s):
+        b = ker((mat - ev*I)**m)
+        if len(b)<m:
+            raise PrecisionError("Cannot compute a basis of this generalized eigenspace. ")
+        space = {'eigenvalue' : ev, 'multiplicity' : m, 'basis' : b}
+        if projections:
+            space['projection'] = proj[i]
+        GenEigSpaces.append(space)
 
     return GenEigSpaces
--- polynomials.py
+++ polynomials.py
@@ -9,7 +9,7 @@ def clean_pol(pol):
 
 def GCD(a, b):
 
-    """
+    r"""
     Return a *non-rigorous* gcd of the polynomials "a" and "b".
 
     Note: this function is designed for BallField as base ring.
@@ -44,8 +44,48 @@ def GCD(a, b):
 
 
 
-def XGCD():
-    return
+def XGCD(a, b):
+
+    r"""
+    Return a *non-rigorous* gcd of the polynomials "a" and "b" and the
+    coefficients in the Bezout identity.
+
+    Note: this function is designed for BallField as base ring.
+
+    Some words about the correction of this function:
+    Let a· and b· be fixed.
+
+    INPUT:
+
+     -- "a" -- polynomial
+     -- "b" -- polynomial
+
+    OUTPUT:
+
+     -- "d" -- polynomial
+     -- "u" -- polynomial
+     -- "v" -- polynomial
+
+    """
+
+    P = a.parent()
+
+    a, b = clean_pol(a), clean_pol(b)
+    if a==0: return b, P.zero(), P.one()
+    if b==0: return a, P.one(), P.zero()
+    if a.degree() < b.degree():
+        d, v, u = XGCD(b, a)
+        return d, u, v
+
+    r0, u0, v0, r1, u1, v1 = a, P.one(), P.zero(), b, P.zero(), P.one()
+    while r1!=0:
+        r0, (q, r1) = r1, r0.quo_rem(r1)
+        r1 = clean_pol(r1)
+        u0, v0, u1, v1 = u1, v1, u0 - q*u1, v0 - q*v1
+
+    d, u, v = r0, clean_pol(u0), clean_pol(v0)
+
+    return d, u, v
 
 
 
