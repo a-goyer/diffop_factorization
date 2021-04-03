0c13072f6803ba7d7c86207d7f8ee179ba705ff5
a-goyer <alexandre.goyer@inria.fr>
2021-04-03 17:01:22 +0200

    completing eigenspaces and polynomials modules

--- eigenspaces.py
+++ eigenspaces.py
@@ -5,7 +5,7 @@ from sage.matrix.special import identity_matrix
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 from sage.misc.misc_c import prod
 
-def eigenvalues(mat, multiplicities=False):
+def eigenvalues(mat, *, multiplicities=False):
 
     r"""
     Return the eigenvalues of "mat".
@@ -29,7 +29,17 @@ def eigenvalues(mat, multiplicities=False):
     ev a complex number and m a positive integer.
 
 
-    EXAMPLES:
+    EXAMPLE::
+
+        sage: from diffop_factorization.eigenspaces import eigenvalues
+        sage: r2, r3 = CBF(sqrt(2)), CBF(sqrt(3))
+        sage: mat = jordan_block(r2, 2).block_sum(matrix([r3]))
+        sage: ran = MatrixSpace(CC, 3).random_element().change_ring(CBF)
+        sage: mat = ~ran * mat * ran
+        sage: eigenvalues(mat, multiplicities=True)
+        [([1.4142136 +/- 5.47e-8] + [+/- 1.71e-8]*I, 2),
+         ([1.7320508 +/- 2.65e-8] + [+/- 1.89e-8]*I, 1)]
+
     """
 
     p = mat.charpoly()
@@ -60,7 +70,7 @@ def eigenvalues(mat, multiplicities=False):
 
 
 
-def gen_eigenspaces(mat, projections=False):
+def gen_eigenspaces(mat, *, projections=False):
 
     r"""
     Return the generalized eigenspaces of "mat".
@@ -95,11 +105,48 @@ def gen_eigenspaces(mat, projections=False):
     "mat", whose keys are the following strings:
      - 'eigenvalue'   : complex number
      - 'multiplicity' : integer
-     - 'basis' : list of vectors
+     - 'basis'        : list of vectors
      - 'projection'   : polynomial (if 'projections=True' is specified).
 
 
     EXAMPLES:
+
+    A generic example ::
+
+        sage: from diffop_factorization.eigenspaces import gen_eigenspaces
+        sage: mat = MatrixSpace(CC, 3).random_element().change_ring(CBF)
+        sage: gen_eigenspaces(mat)
+        [{'eigenvalue': [0.008731294334 +/- 5.79e-13] + [-0.457554487127 +/- 3.74e-13]*I,
+          'multiplicity': 1,
+          'basis': [([-0.081853906993 +/- 6.52e-13] + [-0.087179008676 +/- 7.81e-13]*I, 1.000000000000000, [-0.342339920094 +/- 3.62e-13] + [0.098986705031 +/- 3.30e-13]*I)]},
+         {'eigenvalue': [0.339079560015 +/- 4.32e-13] + [0.888549954437 +/- 3.85e-13]*I,
+          'multiplicity': 1,
+          'basis': [([-0.409699282668 +/- 6.41e-13] + [0.247473920868 +/- 5.86e-13]*I, [-0.066871473678 +/- 7.28e-13] + [0.685673697712 +/- 7.82e-13]*I, 1.000000000000000)]},
+         {'eigenvalue': [0.606834685948 +/- 7.61e-13] + [-0.992982372445 +/- 8.72e-13]*I,
+          'multiplicity': 1,
+          'basis': [([-0.63246660126 +/- 2.18e-12] + [-0.05887072347 +/- 3.44e-12]*I, 1.000000000000000, [-0.59541382749 +/- 5.20e-12] + [0.10852249946 +/- 5.37e-12]*I)]}]
+
+    An example with a multiple eigenvalue ::
+
+        sage: from diffop_factorization.eigenspaces import gen_eigenspaces
+        sage: r2, r3 = CBF(sqrt(2)), CBF(sqrt(3))
+        sage: mat = jordan_block(r2, 2).block_sum(matrix([r3]))
+        sage: ran = MatrixSpace(CC, 3).random_element().change_ring(CBF)
+        sage: mat = ~ran * mat * ran
+        sage: GenEigSpaces = gen_eigenspaces(mat, projections=True)
+        sage: [(space['eigenvalue'], space['multiplicity']) for space in GenEigSpaces]
+        [([1.41421356 +/- 7.84e-9] + [+/- 5.47e-9]*I, 2),
+         ([1.73205081 +/- 8.48e-9] + [+/- 6.05e-9]*I, 1)]
+        sage: ev, vec = GenEigSpaces[1]['eigenvalue'], GenEigSpaces[1]['basis'][0]
+        sage: (mat - ev*identity_matrix(CBF, 3))*vec
+        ([+/- 1.37e-6] + [+/- 1.37e-6]*I, [+/- 4.33e-7] + [+/- 4.33e-7]*I, [+/- 1.17e-6] + [+/- 1.17e-6]*I)
+        sage: T = matrix(GenEigSpaces[0]['basis'] + GenEigSpaces[1]['basis']).transpose()
+        sage: pol = GenEigSpaces[0]['projection']
+        sage: P = ~T * pol(mat) * T; P
+        [  [1.00 +/- 1.19e-4] + [+/- 1.19e-4]*I        [+/- 8.62e-5] + [+/- 8.62e-5]*I        [+/- 1.53e-4] + [+/- 1.53e-4]*I]
+        [       [+/- 3.97e-5] + [+/- 3.97e-5]*I [1.0000 +/- 4.89e-5] + [+/- 4.89e-5]*I        [+/- 5.52e-5] + [+/- 5.52e-5]*I]
+        [       [+/- 9.55e-5] + [+/- 9.55e-5]*I        [+/- 7.19e-5] + [+/- 7.19e-5]*I        [+/- 1.25e-4] + [+/- 1.25e-4]*I]
+
     """
 
     n, C = mat.nrows(), mat.base_ring()
@@ -110,7 +157,7 @@ def gen_eigenspaces(mat, projections=False):
 
     if projections:
         k, l = len(s), len(s)//2
-        P = [(x-ev)*m for ev, m in s]
+        P = [(x-ev)**m for ev, m in s]
         Q = [Pol(prod(P[j] for j in range(k) if j != i)) for i in range(k)]
         d, u1, u2 = XGCD(sum(Q[:l]), sum(Q[l:]))
         proj = [u*q for u, q in zip([u1]*l + [u2]*(k-l), Q)]
--- polynomials.py
+++ polynomials.py
@@ -1,9 +1,12 @@
 
 
-def clean_pol(pol):
+def _clean(pol):
+
     l = pol.coefficients()
     while len(l)>0 and l[-1].contains_zero(): l.pop()
-    return pol.parent()(l)
+    cpol = pol.parent()(l)
+
+    return cpol
 
 
 
@@ -28,17 +31,25 @@ def GCD(a, b):
      -- "a" -- polynomial
 
 
-    EXAMPLES::
+    EXAMPLE::
+
+        sage: from diffop_factorization.polynomials import GCD
+        sage: P.<x> = CBF[]; a = CBF(pi)
+        sage: p, q = (x-1)*(x-2)**2, (x-2)*(x-3)**2
+        sage: p, q = p(x*a), q(x*a)
+        sage: d = GCD(p, q); d(x/a).monic()
+        ([1.0000000000 +/- 1.34e-12])*x + [-2.00000000000 +/- 1.94e-12]
+
     """
 
-    a, b = clean_pol(a), clean_pol(b)
+    a, b = _clean(a), _clean(b)
     if a==0: return b
     if b==0: return a
     if a.degree() < b.degree(): return GCD(b, a)
 
     while b != 0:
         a, b = b, a.quo_rem(b)[1]
-        b = clean_pol(b)
+        b = _clean(b)
 
     return a
 
@@ -47,7 +58,7 @@ def GCD(a, b):
 def XGCD(a, b):
 
     r"""
-    Return a *non-rigorous* gcd of the polynomials "a" and "b" and the
+    Return the *non-rigorous* monic gcd of the polynomials "a" and "b" and the
     coefficients in the Bezout identity.
 
     Note: this function is designed for BallField as base ring.
@@ -66,11 +77,24 @@ def XGCD(a, b):
      -- "u" -- polynomial
      -- "v" -- polynomial
 
+
+    EXAMPLE::
+
+        sage: from diffop_factorization.polynomials import XGCD, _clean
+        sage: P.<x> = CBF[]; a = CBF(pi)
+        sage: p, q = (x-1)*(x-2)**2, (x-2)*(x-3)**2
+        sage: p, q = p(x*a), q(x*a)
+        sage: d, u, v = XGCD(p, q); d, 2/a
+        (([1.0000000000 +/- 1.33e-12])*x + [-0.63661977237 +/- 3.04e-12],
+         [0.636619772367581 +/- 4.28e-16])
+        sage: _clean(u*p + v*q)
+        ([1.000000000 +/- 3.39e-11])*x + [-0.63661977237 +/- 6.95e-12]
+
     """
 
     P = a.parent()
 
-    a, b = clean_pol(a), clean_pol(b)
+    a, b = _clean(a), _clean(b)
     if a==0: return b, P.zero(), P.one()
     if b==0: return a, P.one(), P.zero()
     if a.degree() < b.degree():
@@ -80,10 +104,11 @@ def XGCD(a, b):
     r0, u0, v0, r1, u1, v1 = a, P.one(), P.zero(), b, P.zero(), P.one()
     while r1!=0:
         r0, (q, r1) = r1, r0.quo_rem(r1)
-        r1 = clean_pol(r1)
         u0, v0, u1, v1 = u1, v1, u0 - q*u1, v0 - q*v1
+        r1 = _clean(r1)
 
-    d, u, v = r0, clean_pol(u0), clean_pol(v0)
+    lc = r0.leading_coefficient()
+    d, u, v = r0.monic(), _clean(u0/lc), _clean(v0/lc)
 
     return d, u, v
 
@@ -92,7 +117,7 @@ def XGCD(a, b):
 def radical(pol):
 
     r"""
-    Return a *non-rigorous* radical of the polynomial "pol".
+    Return the *non-rigorous* radical of the polynomial "pol".
 
     Note: this function is designed for BallField as base ring.
 
@@ -111,10 +136,24 @@ def radical(pol):
      -- "rad" -- polynomial
 
 
-    EXAMPLES::
+    EXAMPLE::
+
+        sage: from diffop_factorization.polynomials import radical
+        sage: P.<x> = CBF[]; a = CBF(pi)
+        sage: p = (x-1)*(x-2)**2
+        sage: p = p(x*a).monic(); p
+        ([1.0000000000000 +/- 1.57e-15])*x^3 + ([-1.59154943091895 +/- 5.18e-15])*x^2 + ([0.81056946913870 +/- 2.96e-15])*x + [-0.129006137732798 +/- 1.12e-16]
+        sage: rad = radical(p); rad
+        ([-44.4132198049 +/- 2.39e-11])*x^2 + ([42.4115008235 +/- 7.69e-11])*x + [-9.0000000000 +/- 3.38e-11]
+        sage: rad.roots(multiplicities=False)
+        [[0.3183098862 +/- 2.31e-11] + [+/- 6.86e-12]*I,
+         [0.6366197724 +/- 4.20e-11] + [+/- 9.55e-12]*I]
+        sage: [1/a, 2/a]
+        [[0.318309886183791 +/- 4.43e-16], [0.636619772367581 +/- 4.28e-16]]
+
     """
 
     d = GCD(pol, pol.derivative())
-    rad = clean_pol(pol.quo_rem(d)[0])
+    rad = _clean(pol.quo_rem(d)[0])
 
     return rad
